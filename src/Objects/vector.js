53f3e9dcae3098c1537cde48ad15ba35f2d46150
ClosedSequence <nathanh722@gmail.com>
2018-12-08 20:06:42 -0700

    remove extra js files; cleanup from compile

--- src/Objects/vector.js
+++
@@ -1,9 +0,0 @@
-exports.__esModule = true;
-var Position2D = /** @class */ (function () {
-    function Position2D(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    return Position2D;
-}());
-exports.Position2D = Position2D;
