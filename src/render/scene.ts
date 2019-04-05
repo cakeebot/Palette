671b344ccfdf8ab445d5f1cfeffef6de54fad955
Nate <nathanh722@gmail.com>
2019-04-05 16:39:51 -0700

    Add renderAttributes to Scene class.

--- src/render/scene.ts
+++ src/render/scene.ts
@@ -1,12 +1,34 @@
-import { Shape } from "../Objects/shape";
+import { Shape } from '../Objects/shape';
 import { types } from './../Utility/elixir';
+import { Color } from './../Objects/color';
+
+export enum SceneBGType {
+  Image = "IMAGE",
+  Color = "Color",
+}
+
+module SceneTypes {
+  export interface SceneBG {
+    type: SceneBGType
+    textured: boolean
+  }
+}
 
 export class Scene {
   shapes: Shape[]
   size: types.Size
 
   private privateAttributes = {
-    useWholeSpace: <boolean> true
-    
+    useWholeSpace: <boolean> true,
+  }
+
+  renderAttributes = {
+    BGType: <SceneTypes.SceneBG> {
+      type: <SceneBGType> '',
+      textured: <boolean> false
+    },
+
+    bgImage: <string> '',
+    bgColor: Color
   }
 }
