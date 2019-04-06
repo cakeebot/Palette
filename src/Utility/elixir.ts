fcab1b8f58c719826449843bfcd6f229b53542af
Nate <nathanh722@gmail.com>
2019-04-06 10:00:47 -0700

    Add tag system

--- src/Utility/elixir.ts
+++ src/Utility/elixir.ts
@@ -3,12 +3,32 @@
   used in Palette and can be used by Palette games.
 */
 
+export module tags {
+  export interface TagHolder {
+    tags: Tag[]
+  }
+
+  export function assignTag (tagName: string, obj: TagHolder): void {
+    if (!array.includes(obj.tags, [tagName, 'paletteTag'])) {
+      obj.tags.push([tagName, 'paletteTag'])
+    }
+  }
+
+  export type Tag = [string, 'paletteTag']
+}
+
 export module types {
   export type VoidFunction = () => void
   export type RenderFunction = (progress: number) => void
   export interface Size {
     x: number
     y: number
+  }
+}
+
+export module array {
+  export function includes (arr: any[], obj: any): boolean {
+    return (arr.indexOf(obj) != -1)
   }
 }
 
