f66a82369e23bbc4b1d1afd579bc9fa511e1e64b
Nate <nathanh722@gmail.com>
2019-04-09 16:33:57 -0700

    SceneRender and SceneBG classes, scene constructor

--- src/render/scene.ts
+++ src/render/scene.ts
@@ -1,16 +1,55 @@
 import { Shape } from '../Objects/shape';
-import { types } from './../Utility/elixir';
-import { Color } from './../Objects/color';
+import { types } from '../Utility/elixir';
+import { Color } from '../Objects/color';
 
 export enum SceneBGType {
-  Image = "IMAGE",
-  Color = "Color",
+  Image = 'IMAGE',
+  Color = 'COLOR',
+  Shapes = 'SHAPES'
+}
+
+export enum BGImageType {
+  Textured = 'TEXTURED',
+  Scaled = 'SCALED'
 }
 
 module SceneTypes {
-  export interface SceneBG {
-    type: SceneBGType
-    textured: boolean
+  export class SceneBG {
+    bgType: SceneBGType
+    imageType: BGImageType
+
+    constructor (bgType?: SceneBGType, imageType?: BGImageType) {
+      this.bgType = bgType
+      this.imageType = imageType
+    }
+  }
+}
+
+export class SceneRender {
+  useWholeSpace: boolean
+
+  bgType: SceneTypes.SceneBG = new SceneTypes.SceneBG()
+
+  bgImage: string
+  bgColor: Color
+  bgShapes: Shape[]
+
+  constructor (
+    bgType?: SceneBGType,
+    bgContent?: string | Color | Shape[],
+    useWholeSpace?: boolean,
+    bgImageType?: BGImageType
+  ) {
+    this.bgType.bgType = bgType
+    this.bgType.imageType = bgImageType
+
+    if (typeof bgContent == 'string' && bgType == 'IMAGE') {
+      this.bgImage = 'string'
+    } else if (bgContent instanceof Color && bgType == 'COLOR') {
+      this.bgColor = bgContent
+    } else if (bgContent instanceof Array && bgType == 'SHAPES') {
+      this.bgShapes = bgContent
+    }
   }
 }
 
@@ -18,17 +57,16 @@ export class Scene {
   shapes: Shape[]
   size: types.Size
 
-  private privateAttributes = {
-    useWholeSpace: <boolean> true,
-  }
+  renderAttributes: SceneRender
 
-  renderAttributes = {
-    BGType: <SceneTypes.SceneBG> {
-      type: <SceneBGType> '',
-      textured: <boolean> false
-    },
-
-    bgImage: <string> '',
-    bgColor: Color
+  constructor (
+    size: types.Size | boolean,
+    renderAttributes: SceneRender
+  ) {
+    if (typeof size == 'boolean') {
+      this.renderAttributes.useWholeSpace = size
+    } else {
+      this.size = size
+    }
   }
 }
