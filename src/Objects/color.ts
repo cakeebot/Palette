77017b79d27f1e3027a8dae4864fffe5e96198dd
Nate <nathanh722@gmail.com>
2019-04-09 18:47:24 -0700

    Add alpha value check; remove color redundance

--- src/Objects/color.ts
+++ src/Objects/color.ts
@@ -5,9 +5,11 @@ export class Color {
 
   render: string
 
-  constructor (r: number, g: number, b: number, a: number = 0) {
-    if (r > 255 || g > 255 || b > 255 || a > 255) {
+  constructor (r: number, g: number, b: number, a: number = 1) {
+    if (r > 255 || g > 255 || b > 255) {
       throw new Error('Color: Max value is 255!')
+    } else if (a > 1) {
+      throw new Error ('Color: Max alpha value is 1.0!')
     } else {
       this.r = r
       this.g = g
@@ -17,17 +19,17 @@ export class Color {
     }
   }
 
-  static readonly colors = {
-    red: new Color(255, 0, 0),
-    green: new Color(0, 255, 0),
-    blue: new Color(0, 0, 255),
+  // colors
+  static readonly red: Color = new Color(255, 0, 0)
+  static readonly green: Color = new Color(0, 255, 0)
+  static readonly blue: Color = new Color(0, 0, 255)
 
-    white: new Color(255, 255, 255),
-    black: new Color(0, 0, 0),
-    gray: new Color(128, 128, 128),
+  static readonly white: Color = new Color(255, 255, 255)
+  static readonly black: Color = new Color(0, 0, 0)
+  static readonly gray: Color = new Color(128, 128, 128)
 
-    yellow: new Color(255, 255, 0),
-    pink: new Color(255, 0, 255),
-    purple: new Color(128, 0, 128)
-  }
+  static readonly yellow: Color = new Color(255, 255, 0)
+  static readonly pink: Color = new Color(255, 0, 255)
+  static readonly purple: Color = new Color(128, 0, 128)
+  
 }
