19162c0c3dc889d3e4d38a51a101af673822a540
Nate <nathanh722@gmail.com>
2019-04-26 21:06:31 -0700

    Merge velocity.ts into vector.ts; delete folder src/Physics and velocity.ts

--- src/Objects/vector.ts
+++ src/Objects/vector.ts
@@ -1,3 +1,5 @@
+
+// Position
 export class Position2D {
   x: number
   y: number
@@ -22,3 +24,35 @@ export class Position3D {
 
 export type Coords2D = [number, number]
 export type Coords3D = [number, number, number]
+
+
+// Velocity
+export class Velocity2D {
+  x: number
+  y: number
+
+  constructor(
+    x: number,
+    y: number
+  ) {
+    this.x = x
+    this.y = y
+  }
+}
+
+export class Velocity3D {
+  x: number
+  y: number
+  z: number
+
+  constructor(
+    x: number,
+    y: number,
+    z: number
+  ) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
+}
+
