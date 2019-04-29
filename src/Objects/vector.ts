eda4e862371f1bb10251990f28af9dd3a6777330
Nate <nathanh722@gmail.com>
2019-04-28 21:15:03 -0700

    Coords2D/3D is now Vector2D/3D; added functions to more easily convert these; functions to easily create velocity + positions

--- src/Objects/vector.ts
+++ src/Objects/vector.ts
@@ -22,9 +22,12 @@ export class Position3D {
   }
 }
 
-export type Coords2D = [number, number]
-export type Coords3D = [number, number, number]
-
+export function pos2D (x: number, y: number) {
+  return new Position2D (x, y)
+}
+export function pos3D (x: number, y: number, z: number) {
+  return new Position3D (x, y, z)
+}
 
 // Velocity
 export class Velocity2D {
@@ -56,3 +59,21 @@ export class Velocity3D {
   }
 }
 
+export function vel2D (x: number, y: number) {
+  return new Velocity2D (x, y)
+}
+export function vel3D (x: number, y: number, z: number) {
+  return new Velocity3D (x, y, z)
+}
+
+// Vectors
+export type Vector2D = [number, number]
+export type Vector3D = [number, number, number]
+
+export function toPos2D (vector: Vector2D | Position2D): Position2D {
+  return new Position2D (vector[0], vector[1])
+}
+
+export function toVel2D (vector: Vector2D | Velocity2D): Velocity2D {
+  return new Position2D (vector[0], vector[1])
+}
